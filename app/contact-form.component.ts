de5750ea0b1008b9b6b4487f031c2589ff7cb624
Onur T <onurtumer@gmail.com>
2016-05-21 12:58:44 +0300

    Chapter 7

--- app/contact-form.component.ts
+++ app/contact-form.component.ts
@@ -1,11 +1,18 @@
 import { Component } from 'angular2/core';
+import { Control, ControlGroup, Validators } from 'angular2/common';
+import { UsernameValidators } from './usernameValidators'
 
 @Component({
   selector: 'contact-form',
   templateUrl: 'app/contact-form.component.html' 
 })
 export class FormComponent {
-  onSubmit(form) {
-    console.log(form);
+ form = new ControlGroup({
+   username: new Control('', Validators.compose([Validators.required, UsernameValidators.cannotContainSpace]), 
+                                                 UsernameValidators.shouldBeUnique),
+   password: new Control('', Validators.required)
+ });
+  signup() {
+    console.log(this.form.value);
   }
 }
